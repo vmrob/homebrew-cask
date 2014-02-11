6776f45a2ffe72e093e625db2b50dd5443e4ff13
Roland Walker <walker@pobox.com>
2014-02-11 13:36:55 -0500

    indent should be two spaces, no tabs

--- lib/cask/container/air.rb
+++ lib/cask/container/air.rb
@@ -4,19 +4,19 @@ class Cask::Container::Air < Cask::Container::Base
   end
 
   def extract
-  	executable = '/Applications/Utilities/Adobe AIR Application Installer.app/Contents/MacOS/Adobe AIR Application Installer'
+    executable = '/Applications/Utilities/Adobe AIR Application Installer.app/Contents/MacOS/Adobe AIR Application Installer'
     args = ['-silent', '-location', @cask.destination_path, @path]
-  	
+
     if !Pathname(executable).exist?
-    	raise CaskError.new(<<-ERRMSG)
+      raise CaskError.new(<<-ERRMSG)
 Error installing application: Adobe Air runtime not present, try installing via `brew cask install adobe-air`
     ERRMSG
     end
 
-  	begin
-  	  @command.run!(executable, :args => args)
-  	rescue
-	  raise CaskError.new(<<-ERRMSG)
+    begin
+      @command.run!(executable, :args => args)
+    rescue
+      raise CaskError.new(<<-ERRMSG)
 Error installing application.
   command was:
   #{executable} #{args.map { |arg| arg }.join(' ')}
